22c651ffd58587c4ed51a37ba3b6e3bdb74ab948
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-06-05 15:52:52 +0200

    add js

--- js/myscript.js
+++ js/myscript.js
@@ -1,33 +1,34 @@
-let ticketPrice;
+// il nome dell'utente
+const userName = document.getElementById("name");
 
-// Chiedere all’utente il numero di chilometri che vuole percorrere
-let userKm = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
-// Chiedere all'utente l’età del passeggero
-let userAge = parseInt(prompt("Quanti anni ha il viaggiatore?"));
-
-console.log("I chilometri da percorrere sono: ", userKm);
-console.log("L'età del passegero è: ", userAge);
-
-
-
-// Calcolare il prezzo totale del viaggio
+// il numero di chilometri da percorrere
+const userKm = document.getElementById("km");
 
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-let priceKm = userKm * 0.21;
+const priceKm = userKm.value * 0.21;
+let ticketPrice;
 
-console.log("Il prezzo intero del biglietto è: ", priceKm);
+// età del passeggero
+let userAge = document.querySelector("select");
 
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-if(userAge <= 18) {
+const choice = userAge.value;
+
+if(choice === "under") {
     ticketPrice = ((priceKm * 80) / 100);
-} else if(userAge >= 65) {
-    ticketPrice = ((priceKm * 60) / 100);
-} else {
+}else if(choice === "adult") {
     ticketPrice = priceKm;
+}else {
+    ticketPrice = ((priceKm * 60) / 100);
 }
 
-console.log("Il prezzo finale del biglietto è: ", ticketPrice);
 
-// L’output con massimo due decimali
-document.getElementById("ticket").innerHTML = "€" + ticketPrice.toFixed(2)
+// output con click
+const bottoneCalcola = document.getElementById("calcola");
+
+bottoneCalcola.addEventListener("click",
+    function() {
+        let inputValueName = userName.value;
+        let inputValueKm = userKm.value;
+        console.log(inputValueName, inputValueKm, ticketPrice);
+    }
+)
